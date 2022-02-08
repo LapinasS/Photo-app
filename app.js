67fbbeeac6da5dda4f9411c7ce261b8ab0c3501f
Andrius Lapinas <andriuslapinas@gmail.com>
2022-02-08 11:39:05 +0200

    Photobox

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
+var x = document.getElementById("demo");
 
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
@@ -31,3 +32,35 @@ cameraTrigger.onclick = function() {
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
+
+//get gps
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+  function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+  }
+  
+  function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        x.innerHTML = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+        x.innerHTML = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+        x.innerHTML = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+        x.innerHTML = "An unknown error occurred."
+        break;
+    }
+  }

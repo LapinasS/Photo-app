5a6b1bf570b6d1dc2861b6f4c3b46268c5fe20b7
Andrius Lapinas <andriuslapinas@gmail.com>
2022-02-14 21:35:28 +0200

    Photo-app

--- app.js
+++ app.js
@@ -67,19 +67,18 @@ function getLocation() {
         break;
     }
   }
+const lat = 54.698369;
+const lng = 25.296383;
+let url = `https://geocode.farm/v3/json/reverse/?lat=${lat}&lon=${lng}&country=us&lang=en&count=1`;
 
 async function showAdd() {
-  const lat = 54.698369;
-  const lng = 25.296383;
-  let url = `https://geocode.farm/v3/json/reverse/?lat=${lat}&lon=${lng}&country=us&lang=en&count=1`;
+
   const response = await fetch(url);
   const data = await response.json();  
   const results = await data.geocoding_results.RESULTS[0];
   const resultsAddr = results.ADDRESS;
-  console.log(response.json());
   document.querySelector('.results').innerHTML += `<div>${results.formatted_address}</div>`;
   document.querySelector('.results').innerHTML += `<div>${resultsAddr.admin_2}, ${resultsAddr.street_name}, ${resultsAddr.street_number}.</div>`;
-  
   console.log(data); 
   console.log (results);
   console.log(resultsAddr);
